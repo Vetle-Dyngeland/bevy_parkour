e3b95dc30bc037544edd1554a40b29a7864922b0
= <vetle.dyngeland@gmail.com>
2023-07-16 17:04:56 +0200

    input

--- src/player/input.rs
+++ src/player/input.rs
@@ -7,13 +7,25 @@ pub(super) struct PlayerInputPlugin;
 
 impl Plugin for PlayerInputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PreStartup, init.in_set(PlayerStartupSet::Input));
+        app.add_plugins(InputManagerPlugin::<InputAction>::default())
+            .add_systems(PreStartup, init.in_set(PlayerStartupSet::Input));
     }
 }
 
 #[allow(unused)]
 fn init(mut cmd: Commands, player_query: Query<Entity, With<Player>>) {
-
+    cmd.entity(player_query.single())
+        .insert(InputManagerBundle {
+            input_map: InputMap::new([
+                (KeyCode::Space, InputAction::Jump),
+                (KeyCode::ControlLeft, InputAction::Coil),
+                (KeyCode::ShiftLeft, InputAction::Land),
+            ])
+            .insert(MouseButton::Middle, InputAction::Boost)
+            .insert(VirtualDPad::wasd(), InputAction::Run)
+            .build(),
+            action_state: ActionState::<InputAction>::default(),
+        });
 }
 
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, TypePath)]
